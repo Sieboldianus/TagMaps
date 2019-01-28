d6aae556d91ea4f999067143489f7565f0d935bc
AD <alexander.dunkel@tu-dresden.de>
2019-01-28 12:26:04 +0100

    clean cmd scripts with obsolete args list

--- 00_generateClusters_OnlyPhotoLocations.cmd
+++ 00_generateClusters_OnlyPhotoLocations.cmd
@@ -1,17 +1,2 @@
-#Optional Parameter:
-#-s source, default= "fromLBSN"
-#-r removeLongTail, default= "True"
-#-e EPSGCode, default= Auto Detect!
-#-t clusterTags, default= "True"
-#-p cluster Photos, default= "True"
-#-o cluster Emoji, default= "True"
-#-m topicModeling, default= "False"
-#-j tokenizeJapanese, default= "False"
-#-w writeCleanedData, default= "True"
-#-c localSaturationCheck (will exclude any tags that are used over the whole area), default= "False"
-#-i shapefileIntersect (will clip with referenced shapefile, requires WGS1984 Projection
-#-f shapefilePath
-#-stat statisticsOnly
-#-lmuc limitBottomUserCount, default = 5
 %~dp0tagmaps.exe -o -t
 exit
