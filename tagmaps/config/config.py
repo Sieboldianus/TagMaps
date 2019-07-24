bb8abce55097d14cc97a976aef37654e4baceed5
Alexander Dunkel <alexander.dunkel@tu-dresden.de>
2019-07-24 13:09:55 +0200

    refactor: input args follow syntax conventions

--- tagmaps/config/config.py
+++ tagmaps/config/config.py
@@ -26,7 +26,7 @@ class BaseConfig:
     def __init__(self):
         # Set Default Config options here
         # or define options as input args
-        self.data_source = "fromLBSN"
+        self.data_source = "lbsn"
         self.cluster_tags = True
         self.cluster_locations = True
         self.epsg = True
@@ -95,32 +95,32 @@ class BaseConfig:
                             version=f'tagmaps {__version__}')
         parser.add_argument("-s",
                             "--source",
-                            default="fromLBSN",
+                            default="lbsn",
                             help="Specify type of source of data. "
                             "This is needed to read data. "
-                            "Defaults to 'fromLBSN'.")
+                            "Defaults to 'lbsn'.")
         parser.add_argument("-r",
-                            "--disableRemoveLongTail",
+                            "--disable_remove_longtail",
                             action="store_true",
                             help="This will disable exclusion"
                             "of tags that are used by only a "
                             "small number of users")
         parser.add_argument("-e",
-                            "--EPSG",
+                            "--epsg",
                             help="If provided, will overwrite "
                             "auto EPSG code")
-        parser.add_argument("-t", "--disableClusterTags",
+        parser.add_argument("-t", "--disable_cluster_tags",
                             action="store_true",
                             help="Disable cluster tag locations"
                             "No clusters will be generated per "
                             "distinct tag.")
         parser.add_argument("-p",
-                            "--disableClusterLocations",
+                            "--disable_cluster_locations",
                             action="store_true",
                             help="Disable cluster of post locations "
                             )
         parser.add_argument("-c",
-                            "--localSaturationCheck",
+                            "--local_saturation_check",
                             action="store_true",
                             help="Will attempt to exclude any tags that "
                             "are (over)used at above a certain percentage "
@@ -128,28 +128,20 @@ class BaseConfig:
                             "may improve legibility of tag maps at "
                             "larger scales"
                             )
-        # parser.add_argument("-j",
-        #                     "--tokenizeJapanese",
-        #                     action="store_true",
-        #                     help="Japanese language requires "
-        #                     "tokenization prior to clustering. "
-        #                     "Disabled by default. Note:"
-        #                     "Not fully implemented"
-        #                     )
         parser.add_argument("-o",
-                            "--disableClusterEmoji",
+                            "--disable_cluster_emoji",
                             action="store_true",
                             help="Disable cluster of "
                             "emoji in addition to tags.")
         parser.add_argument("-m",
-                            "--topicModeling",
+                            "--topic_modeling",
                             action="store_true",
                             help="This will used topic modeling "
                             "to detect groups of tags (based on "
                             "Latent Dirichlet Allocation). "
                             "[Not implemented]")
         parser.add_argument("-w",
-                            "--disableWriteCleanedData",
+                            "--disable_write_cleaneddata",
                             action="store_true",
                             help="This will disable "
                             "write out of a file limited to the data "
@@ -158,56 +150,57 @@ class BaseConfig:
                             "minimum user threshold etc.)"
                             )
         parser.add_argument("-i",
-                            "--shapefileIntersect",
+                            "--shapefile_intersect",
                             action="store_true",
                             help="If set, clip "
                             "data to shapefile (specify path "
                             "with --shapefilePath)"
                             )
         parser.add_argument("-f",
-                            "--shapefilePath",
+                            "--shapefile_path",
                             default="",
                             help="Provide a (full) path to a shapefile "
                             "to clip data prior to clustering.")
-        parser.add_argument("-is",
-                            "--ignoreStoplists",
+        parser.add_argument("-n",
+                            "--ignore_stoplists",
                             action="store_true",
                             help="If stoplist is available "
                             "ignore it."
                             )
-        parser.add_argument("-ip",
-                            "--ignorePlaceCorrections",
+        parser.add_argument("-b",
+                            "--ignore_place_corrections",
                             action="store_true",
                             help="If place corrections are available, "
                             "ignore them."
                             )
-        parser.add_argument("-stat",
-                            "--statisticsOnly",
+        parser.add_argument("-d",
+                            "--statistics_only",
                             action="store_true",
                             help="Do not cluster, only read input data"
                             " and calculate statistics."
                             )
-        parser.add_argument("-lmuc",
-                            "--limitBottomUserCount",
+        parser.add_argument("-l",
+                            "--limit_bottom_usercount",
                             type=int,
                             default=5,
                             help="Remove all tags that are used by "
                             "less than x photographers. Defaults to 5.")
-        parser.add_argument("-wG",
-                            "--disableWriteGISCompLine",
+        parser.add_argument("-g",
+                            "--disable_write_gis_comp_line",
                             action="store_true",
                             default=True,
-                            help="Writes placeholder entry after headerline "
+                            help="Disables writing placeholder "
+                            "entry after headerline "
                             "for avoiding GIS import format issues",
                             )
-        parser.add_argument("-aM",
-                            "--autoMode",
+        parser.add_argument("-a",
+                            "--auto_mode",
                             action="store_true",
                             help="If set, no user input will "
                             "be requested during processing.",
                             )
         parser.add_argument("-fO",
-                            "--filterOrigin",
+                            "--filter_origin",
                             type=str,
                             default=None,
                             help="If provided, will filter input data "
@@ -216,23 +209,23 @@ class BaseConfig:
         parser.add_argument("-v", "--verbose",
                             help="Increase output verbosity",
                             action="store_true")
-        parser.add_argument("-mI", "--maxItems",
+        parser.add_argument("-x", "--max_items",
                             help="Number of distinct items to process",
                             default=1000,
                             type=int)
-        parser.add_argument("-oF", "--outputFolder",
+        parser.add_argument("-q", "--output_folder",
                             help="Complete path for output folder",
                             default="",
                             type=str)
-        parser.add_argument("-iF", "--inputFolder",
+        parser.add_argument("-j", "--input_folder",
                             help="Complete path for input folder",
                             default="",
                             type=str)
-        parser.add_argument("-cF", "--configFolder",
+        parser.add_argument("-k", "--config_folder",
                             help="Complete path for config folder",
                             default="",
                             type=str)
-        parser.add_argument("-lI", "--loadIntermediate",
+        parser.add_argument("-u", "--load_intermediate",
                             help="Load from intermediate (cleaned) data "
                             "from path",
                             default="",
@@ -243,52 +236,52 @@ class BaseConfig:
             self.logging_level = logging.DEBUG
         if args.source:
             self.data_source = args.source
-        if args.disableClusterTags:
+        if args.disable_cluster_tags:
             self.cluster_tags = False
-        if args.disableClusterLocations:
+        if args.disable_cluster_locations:
             self.cluster_locations = False
-        if args.EPSG is None:
+        if args.epsg is None:
             self.override_crs = None
         else:
             self.load_custom_crs(self.epsg)
-        if args.disableRemoveLongTail:
+        if args.disable_remove_longtail:
             self.remove_long_tail = False
-        if args.disableClusterEmoji:
+        if args.disable_cluster_emoji:
             self.cluster_emoji = False
-        if args.topicModeling:
+        if args.topic_modeling:
             self.topic_modeling = True
-        if args.disableWriteCleanedData:
+        if args.disable_write_cleaneddata:
             self.write_cleaned_data = False
-        if args.localSaturationCheck:
+        if args.local_saturation_check:
             self.local_saturation_check = True
-        if args.shapefileIntersect:
+        if args.shapefile_intersect:
             self.shapefile_intersect = True
-        if args.shapefilePath:
-            self.shapefile_path = args.shapefilePath
-        if args.ignoreStoplists:
+        if args.shapefile_path:
+            self.shapefile_path = args.shapefile_path
+        if args.ignore_stoplists:
             self.ignore_stoplists = True
-        if args.ignorePlaceCorrections:
+        if args.ignore_place_corrections:
             self.ignore_place_corrections = True
-        if args.statisticsOnly:
+        if args.statistics_only:
             self.statistics_only = True
-        if args.limitBottomUserCount:
-            self.limit_bottom_user_count = int(args.limitBottomUserCount)
-        if args.disableWriteGISCompLine:
+        if args.limit_bottom_usercount:
+            self.limit_bottom_user_count = int(args.limit_bottom_usercount)
+        if args.disable_write_gis_comp_line:
             self.write_gis_comp_line = False
-        if args.autoMode:
-            self.auto_mode = args.autoMode
-        if args.filterOrigin:
-            self.filter_origin = args.filterOrigin
-        if args.maxItems:
-            self.max_items = args.maxItems
-        if args.outputFolder:
-            self.output_folder = args.outputFolder
-        if args.inputFolder:
-            self.input_folder = args.inputFolder
-        if args.configFolder:
-            self.config_folder = args.configFolder
-        if args.loadIntermediate:
-            self.load_from_intermediate = args.loadIntermediate
+        if args.auto_mode:
+            self.auto_mode = args.auto_mode
+        if args.filter_origin:
+            self.filter_origin = args.filter_origin
+        if args.max_items:
+            self.max_items = args.max_items
+        if args.output_folder:
+            self.output_folder = args.output_folder
+        if args.input_folder:
+            self.input_folder = args.input_folder
+        if args.config_folder:
+            self.config_folder = args.config_folder
+        if args.load_intermediate:
+            self.load_from_intermediate = args.load_intermediate
 
     def load_filterlists(self):
         """Load filterlists for filtering terms (instring and full match)
