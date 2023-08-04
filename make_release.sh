d871acb1c4cd1694f18b0570d9df50c30582b6af
Alexander Dunkel <alexander.dunkel@tu-dresden.de>
2023-08-04 08:48:47 +0200

    build: Add build and release script for semantic-release >=8.0.0

---
+++ make_release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+################################################################################
+#
+# Shell script to make release
+# 1. version and build package
+# 2. upload to pypi
+# 3. publish to VCS (Github releases)
+#
+################################################################################
+
+# Exit as soon as a command fails
+set -e
+
+semantic-release version -v
+twine upload dist/*
+semantic-release publish -v
